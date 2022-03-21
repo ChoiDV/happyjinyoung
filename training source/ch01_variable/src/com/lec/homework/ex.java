d3a12e81d9d438475a6a7207b59935bd76c5977c
Choijinyoung-9878 <chlwlsdud69@naver.com>
2022-03-21 23:37:25 +0900

    2022-03-21

---
+++ training source/ch01_variable/src/com/lec/homework/ex.java
@@ -0,0 +1,28 @@
+package com.lec.homework;
+
+public class ex {
+
+	// 국어, 영어, 수학 점수를 변수에 할당하고 각 점수를 출력하고 총점, 평균 출력하는 프로그램을 구현 하시오
+	
+	public static void main(String[] args) {
+		
+		int korean = 100;
+		int english = 100;
+		int math = 100;
+		
+		System.out.println("국어 점수 = " + korean  + "점");
+		System.out.println("영어 점수 = " + english + "점");
+		System.out.println("수학 점수 = " + math + "점");
+		
+		int sum = korean + english + math;    // 총점  
+		
+		double average = (sum/3.0); 		 	  // 평균 
+		
+		System.out.println("국어,영어,수학의 총점 = "+sum + "점");
+		System.out.println("국어,영어,수학의 평균 = " +average + "점");
+		
+		
+
+	}
+
+}
